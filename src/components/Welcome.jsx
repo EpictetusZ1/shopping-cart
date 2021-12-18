c018a4fe4b7fbabf0f0fb3b62e931d4eb54cd847
J Heaton <jackrobertheaton@gmail.com>
2021-12-17 21:04:01 -0500

    Add lorem to welcome page, update styles

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -5,12 +5,23 @@ import {Link} from "react-router-dom";
 const Welcome = () => {
     return (
         <div className={styles.welcomePage}>
-            <h2 className={styles.heroHeader}>Prints of high quality photos straight from NASA</h2>
+            <div className={styles.welcomePageMain}>
+                <h2 className={styles.heroHeader}>Prints of high quality photos straight from NASA</h2>
                 <div className={styles.buttonContainer}>
                     <Link to={"/shop"}>
                         <button className={styles.heroCTA}>Shop Now</button>
                     </Link>
                 </div>
+            </div>
+            <div className={styles.about}>
+                <div className={styles.aboutTitle}>
+                    About Us:
+                </div>
+                <div className={styles.aboutText}>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae cum delectus dolor doloribus eos hic libero obcaecati possimus quasi, quibusdam sequi tempora ut veniam voluptate voluptates. Accusantium adipisci, alias aliquid, animi architecto, assumenda dolorem doloremque est eveniet facere fugiat hic magni necessitatibus nemo nesciunt nihil nisi nulla quam quasi quibusdam repellat sapiente veritatis voluptate voluptates voluptatum! A ad aliquid autem culpa deleniti, dolore eaque earum enim esse explicabo facere illum ipsum iste iusto laborum minus natus nesciunt, non quaerat quisquam ratione sint voluptatum! Asperiores eos error eveniet fugiat ipsam iure libero minus mollitia nihil optio pariatur quasi rem, repellendus saepe ullam velit veniam voluptate. Aliquid animi aspernatur aut cum, deserunt dicta doloremque dolorum, esse excepturi hic illum impedit inventore ipsa itaque labore laborum magni molestiae necessitatibus nihil nisi quaerat sequi suscipit? Asperiores fuga magnam molestiae praesentium quas quibusdam rem sapiente tempora ullam vitae! Alias culpa eveniet iure minima quas vero.
+
+                </div>
+            </div>
         </div>
     );
 }
